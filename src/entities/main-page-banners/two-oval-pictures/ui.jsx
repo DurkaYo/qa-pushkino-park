5926c62b41742cf36df85eded4b2f6f3166f2ac5
Eduard Nabiullin <e.nabiullin@cheil.com>
2024-03-27 10:25:03 +0300

    WIP main banners

---
+++ src/entities/main-page-banners/two-oval-pictures/ui.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import styles from './styles.module.scss';
+
+export function MainPageBannerTwoOvalPictures() {
+    return (
+        <div className={styles['banner-two-oval-pictures']}>
+            <div className={`${styles['banner-two-oval-pictures']}__container`}>
+                <div className={styles['banner-two-oval-pictures__grid-wrapper']}>
+                    <div className={styles['banner-two-oval-pictures__description']}>
+                        <div className={styles['banner-two-oval-pictures__heading']}>Зимний SaLE</div>
+                        <div className={styles['banner-two-oval-pictures__text']}>Позволь себе больше в новом году</div>
+                        <Link
+                            href='#!'
+                            className={`
+                            ${styles['banner-two-oval-pictures__link']}
+                            btn
+                        `}
+                        >
+                            Подробнее
+                        </Link>
+                    </div>
+                    <div className={styles['banner-two-oval-pictures__images']}>
+                        <img src="/images/temp_dev/main-page/main-banner/two-oval-pictures/1.jpg" alt=""/>
+                        <img src="/images/temp_dev/main-page/main-banner/two-oval-pictures/2.jpg" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/widgets/main-page/banners/ui.jsx
+++ src/widgets/main-page/banners/ui.jsx
@@ -1,13 +1,11 @@
-import styles from './styles.module.scss';
+import {MainPageBannerTwoOvalPictures} from "../../../entities/main-page-banners/two-oval-pictures";
 
-import {UISection} from "../../../shared/ui/section";
+import styles from './styles.module.scss';
 
 export function MainPageBanners() {
     return (
-        <UISection defaultClass={styles['banners']}>
-            <div style={{padding: '30vh 0 5vh 0', fontSize: '1.8rem', background: 'green'}}>
-                Main Page Banners
-            </div>
-        </UISection>
+        <>
+            <MainPageBannerTwoOvalPictures />
+        </>
     )
 }
